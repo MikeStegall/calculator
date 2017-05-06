7d4bf71d28e07038655d16cce1d254c4f20b1471
MikeStegall <mestegall5@gmail.com>
2017-05-06 09:52:57 -0500

    putting the numbers into the input div is now working.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+/* global $ */
+let input = $('.input p')
+let number = $('.box p')
 
+function clearInput () {
+  input.html('')
+}
+
+function injectNumbersintoInput (e) {
+  // input.html(number)
+  // console.log(number.html())
+  let num = e.target.innerHTML
+  input.html(num)
+  console.log(e.target.innerHTML)
+}
+
+$('.box').on('click', injectNumbersintoInput)
+$('.clear').on('click', clearInput)
