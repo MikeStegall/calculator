029fb2ea39f568a521cd30e1b649fc5555f57968
MikeStegall <mestegall5@gmail.com>
2017-05-14 17:34:07 -0500

    Redone the entire code to try and fit the assignment by using fat arrow functions.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,70 @@
 /* global $ */
 const input = $('.input p')
-const box = $('.box p')
+let functions = ''
+let inputNumbersArr = []
 let inputNumbers = ''
-let operators = ['+', '-', '*', '/']
-let num = parseInt(inputNumbers, 10)
+let calcInputArr = []
 
 function clearInput () {
+  inputNumbersArr = []
   input.html('')
-  inputNumbers = ''
 }
-function equal () {
-  let num = parseInt(inputNumbers, 10)
-  // console.log(typeof num)
-  console.log(num)
-}
-function injectNumbersintoInput (e) {
-  let number = e.target.innerHTML
-  if (number !== '=') {
-    inputNumbers += number // keep track of numbers being inputed
-  } else {
-    equal()
-  }
-  input.html(inputNumbers)
+function calculateResult () {
+  let result = calcInputArr.reduce((accu, currentValue, index, arr) => {
+    let parsednum = parseFloat(arr[index + 1])
+    accu = parseFloat(accu)
+    console.log(currentValue)
+    if (typeof currentValue === 'string') {
+      console.log(currentValue)
+      if (currentValue === '+') {
+        accu += parsednum
+      } else if (currentValue === '-') {
+        accu -= parsednum
+      } else if (currentValue === '*') {
+        accu *= parsednum
+      } else if (currentValue === '/') {
+        accu /= parsednum
+      }
+    }
+    console.log(currentValue)
+    return accu
+  }, calcInputArr[0])
+  return result
 }
 
-box.on('click', injectNumbersintoInput)
+$('.numbers p').click((e) => {
+  inputNumbers += e.target.innerHTML
+  input.html(inputNumbers)
+  enableFuntions()
+})
+
+$('.function p').click((e) => {
+  functions = e.target.innerHTML
+  inputNumbersArr.push(inputNumbers, functions)
+  console.log(inputNumbersArr)
+  input.html(inputNumbersArr)
+  inputNumbers = ''
+  disableFuntions()
+})
+$('.equals').click(() => {
+  inputNumbersArr.push(inputNumbers)
+  console.log(inputNumbersArr)
+  let result = calculateResult()
+  input.html(result)
+  disableFuntions()
+  console.log(result)
+  inputNumbersArr = []
+  inputNumbers = result
+  enableFuntions()
+})
+
+function disableFuntions () {
+  $('.function').attr('disabled', true)
+  // console.log('functions are disabled')
+}
+function enableFuntions () {
+  $('.function').attr('disabled', false)
+  // console.log('functions are enabled')
+}
+disableFuntions()
 $('.clear').on('click', clearInput)
