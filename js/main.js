8de7b52c72dcea72d70fe1d50ea0e72eedcc37b1
MikeStegall <mestegall5@gmail.com>
2017-05-14 20:16:18 -0500

    Fixed an issue with it returning undefined, fixed to where you can add a decimal to the end of it, also gave the html a title.

--- js/main.js
+++ js/main.js
@@ -3,68 +3,52 @@ const input = $('.input p')
 let functions = ''
 let inputNumbersArr = []
 let inputNumbers = ''
-let calcInputArr = []
 
 function clearInput () {
+  inputNumbers = ''
   inputNumbersArr = []
   input.html('')
 }
+$('.decimal p').click((e) => {
+  inputNumbers = inputNumbers + '.'
+  input.html(inputNumbers)
+})
+
 function calculateResult () {
-  let result = calcInputArr.reduce((accu, currentValue, index, arr) => {
+  let result = inputNumbersArr.reduce((accu, currentValue, index, arr) => {
     let parsednum = parseFloat(arr[index + 1])
     accu = parseFloat(accu)
-    console.log(currentValue)
-    if (typeof currentValue === 'string') {
-      console.log(currentValue)
-      if (currentValue === '+') {
-        accu += parsednum
-      } else if (currentValue === '-') {
-        accu -= parsednum
-      } else if (currentValue === '*') {
-        accu *= parsednum
-      } else if (currentValue === '/') {
-        accu /= parsednum
-      }
+    if (currentValue === '+') {
+      accu += parsednum
+    } else if (currentValue === '-') {
+      accu -= parsednum
+    } else if (currentValue === '*') {
+      accu *= parsednum
+    } else if (currentValue === '/') {
+      accu /= parsednum
     }
-    console.log(currentValue)
     return accu
-  }, calcInputArr[0])
+  }, inputNumbersArr[0])
   return result
 }
 
 $('.numbers p').click((e) => {
   inputNumbers += e.target.innerHTML
   input.html(inputNumbers)
-  enableFuntions()
 })
 
 $('.function p').click((e) => {
   functions = e.target.innerHTML
   inputNumbersArr.push(inputNumbers, functions)
-  console.log(inputNumbersArr)
   input.html(inputNumbersArr)
   inputNumbers = ''
-  disableFuntions()
 })
 $('.equals').click(() => {
   inputNumbersArr.push(inputNumbers)
-  console.log(inputNumbersArr)
   let result = calculateResult()
   input.html(result)
-  disableFuntions()
-  console.log(result)
   inputNumbersArr = []
   inputNumbers = result
-  enableFuntions()
 })
 
-function disableFuntions () {
-  $('.function').attr('disabled', true)
-  // console.log('functions are disabled')
-}
-function enableFuntions () {
-  $('.function').attr('disabled', false)
-  // console.log('functions are enabled')
-}
-disableFuntions()
 $('.clear').on('click', clearInput)
